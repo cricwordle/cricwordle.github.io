483442cbaad08d311f74c6adcbc893edc68fa6f4
Nikhil N G <nikhil.ng1999@gmail.com>
2025-09-15 13:10:49 +0530

    Build and deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/cricket-wordle/", // important for GitHub Pages
+  base: '/', // Ensure assets are linked correctly for GitHub Pages
 });
